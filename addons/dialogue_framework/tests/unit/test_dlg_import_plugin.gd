287311bbd427183d562026df54703ce98f0a6681
bioartgames <bioartgames@gmail.com>
2026-07-08 01:14:38 -0500

    test(dialogue-framework): Add import manifest warning integration tests

--- addons/dialogue_framework/tests/unit/test_dlg_import_plugin.gd
+++ addons/dialogue_framework/tests/unit/test_dlg_import_plugin.gd
@@ -4,7 +4,9 @@ extends GutTest
 const SOURCE_PATH := "user://gut_dlg_import_test.dlg"
 const SAVE_PATH := "user://gut_dlg_import_test_save.tres"
 const FLAG_SETTING := "dialogue_framework/flag_manifest_path"
+const COMMAND_SETTING := "dialogue_framework/command_manifest_path"
 const DEFAULT_FLAG_PATH := "res://game/dialogue_demo/resources/flag_manifest.tres"
+const DEFAULT_COMMAND_PATH := "res://game/dialogue_demo/resources/command_manifest.tres"
 
 
 func before_each() -> void:
@@ -14,6 +16,7 @@ func before_each() -> void:
 
 func after_each() -> void:
 	ProjectSettings.set_setting(FLAG_SETTING, DEFAULT_FLAG_PATH)
+	ProjectSettings.set_setting(COMMAND_SETTING, DEFAULT_COMMAND_PATH)
 	_cleanup_paths()
 
 
@@ -50,4 +53,37 @@ func test_import_warns_when_flag_manifest_path_missing() -> void:
 	_write_source("~ start\nRoll: Hi.\n=> END\n")
 	var result: Error = DlgImportPlugin.import_source(SOURCE_PATH, SAVE_PATH)
 	assert_eq(result, OK)
-	assert_push_warning("Dlg import warning")
+	assert_push_warning("FlagManifest path is not configured")
+
+
+func test_import_warns_and_succeeds_with_undeclared_flag_when_manifest_path_missing() -> void:
+	ProjectSettings.set_setting(FLAG_SETTING, "")
+	_write_source(
+		'~ start\nif flag("undeclared_flag"):\n    Roll: Hidden.\nRoll: Hi.\n=> END\n'
+	)
+	var result: Error = DlgImportPlugin.import_source(SOURCE_PATH, SAVE_PATH)
+	assert_eq(result, OK)
+	assert_push_warning("FlagManifest path is not configured")
+	assert_true(FileAccess.file_exists(SAVE_PATH))
+
+
+func test_import_warns_with_manifest_path_when_flag_manifest_fails_to_load() -> void:
+	const BAD_FLAG_PATH := "res://nonexistent/gut_flag_manifest.tres"
+	ProjectSettings.set_setting(FLAG_SETTING, BAD_FLAG_PATH)
+	_write_source('~ start\nRoll: Hello {undeclared_brace}.\n=> END\n')
+	var result: Error = DlgImportPlugin.import_source(SOURCE_PATH, SAVE_PATH)
+	assert_eq(result, OK)
+	assert_push_warning("FlagManifest failed to load")
+	assert_push_warning(BAD_FLAG_PATH)
+	assert_true(FileAccess.file_exists(SAVE_PATH))
+
+
+func test_import_warns_with_manifest_path_when_command_manifest_fails_to_load() -> void:
+	const BAD_COMMAND_PATH := "res://nonexistent/gut_command_manifest.tres"
+	ProjectSettings.set_setting(COMMAND_SETTING, BAD_COMMAND_PATH)
+	_write_source("~ start\n@wait 1.0\nRoll: Hi.\n=> END\n")
+	var result: Error = DlgImportPlugin.import_source(SOURCE_PATH, SAVE_PATH)
+	assert_eq(result, OK)
+	assert_push_warning("CommandManifest failed to load")
+	assert_push_warning(BAD_COMMAND_PATH)
+	assert_true(FileAccess.file_exists(SAVE_PATH))
